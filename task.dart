629bd472ac474ec0edb36b79f5a98b5b9c0bc763
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-07-11 14:57:48 +0530

    Add files via upload

---
+++ task.dart
@@ -0,0 +1,12 @@
+void main() {
+  num score = 99;
+  if (score >= 90) {
+    print('Excellent');
+  } else if (score >= 75) {
+    print('Very Good');
+  } else if (score >= 50) {
+    print('good');
+  } else {
+    print('Fail');
+  }
+}
