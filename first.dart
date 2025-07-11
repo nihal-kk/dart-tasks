1e1ab17d1dbfaacb554c0fd4331b86421fc4de59
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-07-11 14:59:53 +0530

    Add files via upload

---
+++ first.dart
@@ -0,0 +1,28 @@
+void main() {
+  int days = 5;
+  switch (days) {
+    case 1:
+      print("monday");
+      break;
+    case 2:
+      print("tuesday");
+      break;
+    case 3:
+      print("wednesday");
+      break;
+    case 4:
+      print("thursday");
+      break;
+    case 5:
+      print("friday");
+      break;
+    case 6:
+      print("saturday");
+      break;
+    case 7:
+      print("sunday");
+      break;
+    default:
+      print("this is not a day");
+  }
+}
