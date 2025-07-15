8f486c19e1706243fcb40a695ac7f9577a041785
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-07-15 10:48:09 +0530

    Add files via upload

---
+++ day-3-1.dart
@@ -0,0 +1,4 @@
+void main() {
+  var name = "muhammed nihal";
+  print("hello,$name");
+}
---
+++ day-3-2.dart
@@ -0,0 +1,8 @@
+void main() {
+  var userName = "Nihal";
+  if (userName[0] == userName[0].toUpperCase()) {
+    print("Valid Name");
+  } else {
+    print("Invalid Name");
+  }
+}
