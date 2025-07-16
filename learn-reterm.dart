35eef99e35f0103965c526211fa669d87116f54c
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-07-16 12:53:26 +0530

    Add files via upload

---
+++ learn-reterm.dart
@@ -0,0 +1,25 @@
+// -----------------------------learn retern in dart-----------------------------------------
+
+void main() {
+   print( getFullName());
+
+}
+String getFullName (){
+String fisrtName ="muhammed ";
+String lastName ="nihal";
+return fisrtName +lastName;
+}
+
+void mai() {
+  print(isPositive(-12));
+}
+
+bool isPositive(int a) {
+  if (a > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
