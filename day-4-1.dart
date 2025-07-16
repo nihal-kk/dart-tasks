3cc61c5bde27f5a15b0008c470910c3b96e35426
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-07-16 14:56:33 +0530

    Add files via upload

---
+++ day-4-1.dart
@@ -0,0 +1,45 @@
+
+
+// ---------------------------------------function practice-----------------------------------------------------------------
+void printName (){
+  print("muhammed nihal");
+  print("ahil shinas");
+  print ("aiman");
+  var i =10;
+  if (i>0){
+    print("positive");
+  }else{
+    print("negetive");
+  }
+}
+//----------- main----------------- main-------------------- main------------------------
+void main() {
+  printName ();
+
+    add(9, 1);
+  add(3, 1);
+  add(11, 1);
+
+   var innu = 10000;
+ var jinnu =10.9;
+ var xey = 45.6;
+
+ res(innu, jinnu, xey);
+}
+
+//------------------------------------------------------------
+
+void add (int a,int b){
+int sum = (a+b)+(a+b);
+print("total : $sum");
+}
+
+//----------------------------------------------------------------------
+
+void res (var i,var j,var x){
+  var rsl =i+j+x;
+  print("TOTAL PRICE IS $rsl");
+}
+
+//-----------------------------------------------------------------------
+
