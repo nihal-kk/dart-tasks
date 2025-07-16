1a89290ee924632a042ad45341d237d2c3d7de56
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-07-16 15:45:38 +0530

    Add files via upload

---
+++ type-of-functions.dart
@@ -0,0 +1,32 @@
+// no p ,no r------------------------------------------------------
+
+void myName (){
+  print("muhammed nihal");
+}
+void main (){
+  myName();
+ fName(1);
+ num name =e();
+ print("my age is $name");
+ print(hello(11, 5));
+}
+
+//p ,no r
+
+void fName(num ni) {
+  var nh = ni + ni;
+  print(nh);
+}
+
+//no p,r
+
+num e() {
+  return 18;
+}
+ 
+//p, r
+
+int hello (int a,int e){
+  var ans =a+e;
+  return ans;
+}
